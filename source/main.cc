5e394fc0abbcfff60c1d7ba1797cf2dc24eed94c
Yuchen Dai <silentdai@gmail.com>
2020-09-05 21:19:39 -0700

    add logs at promise codes
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- source/main.cc
+++ source/main.cc
@@ -1,7 +1,8 @@
-#include <chrono>
+//#include <chrono>
 #include <experimental/coroutine>
 #include <future>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,10 +10,13 @@ class resumable {
 public:
   struct promise_type;
   using coro_handle = std::experimental::coroutine_handle<promise_type>;
-  resumable(coro_handle handle) : handle_(handle) {}
+  resumable(coro_handle handle) : handle_(handle) {
+    std::cout << "construct resumable from hande " << static_cast<const void*>(&handle)
+              << std::endl;
+  }
   // resumable(resumable&) = delete;
   // resumable(resumable&&) = delete;
-  bool resume() {
+  bool resumeXX() {
     if (not handle_.done())
       handle_.resume();
     return not handle_.done();
@@ -25,9 +29,19 @@ private:
 
 struct resumable::promise_type {
   using coro_handle = std::experimental::coroutine_handle<promise_type>;
-  auto get_return_object() { return coro_handle::from_promise(*this); }
-  auto initial_suspend() { return std::experimental::suspend_always(); }
-  auto final_suspend() { return std::experimental::suspend_always(); }
+  auto get_return_object() {
+    std::cout << __FUNCTION__ << std::endl;
+
+    return coro_handle::from_promise(*this);
+  }
+  auto initial_suspend() {
+    std::cout << __FUNCTION__ << std::endl;
+    return std::experimental::suspend_always();
+  }
+  auto final_suspend() {
+    std::cout << __FUNCTION__ << std::endl;
+    return std::experimental::suspend_always();
+  }
   void return_void() {}
   void unhandled_exception() { std::terminate(); }
 };
@@ -35,12 +49,24 @@ struct resumable::promise_type {
 resumable foo() {
   std::cout << "Hello" << std::endl;
   co_await std::experimental::suspend_always();
+  std::cout << "another co_wait" << std::endl;
+
+  co_await std::experimental::suspend_always();
+
   std::cout << "Coroutine" << std::endl;
 }
 
 int main() {
-  resumable res = foo();
-  while (res.resume())
-    ;
+  resumable res1 = foo();
+  resumable res2 = foo();
+  std::vector<resumable*> rs{&res1, &res2};
+  std::vector<bool> runs{true, true};
+  while (std::any_of(runs.begin(), runs.end(), [](auto b) { return b; })) {
+    for (int i = 0; i < 2; i++) {
+      if (runs[i]) {
+        runs[i] = rs[i]->resumeXX();
+      }
+    }
+  }
   return 0;
 }
